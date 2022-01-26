28734a65ff9b5f1db6645a4b185c1a1fa077f149
Goal000330 <ericsmith.dev101@gmail.com>
2022-01-25 20:27:13 -0800

    complete OurTokens component.

--- components/OurTokens.tsx
+++ components/OurTokens.tsx
@@ -5,26 +5,36 @@ const OurTokens = () => {
   return (
     <div className="c-container" id="Our Tokens">
       <div style={{ marginTop: "140px" }} />
-      <h3 className="title-h3" style={{ textAlign: "center" }}>
+      <div className="mainTitle" style={{ textAlign: "left" }}>
+        <div className="dot"></div>
         Our Zilionixx Coin(ZNX)
-      </h3>
-      <h1 className="title-h1" style={{ textAlign: "center" }}>
+      </div>
+      <h1 className="questionTitle" style={{ textAlign: "left" }}>
         What is Zilionixx Coin
       </h1>
-      <div style={{ marginTop: "60px" }} />
+      <div style={{ marginTop: "39px" }} />
       <div style={{ marginBottom: "16px" }}>
         <div className="our-sub-title">
-          <span className="styled-subtitle" style={{ color: "#191f25" }}>
+          <span
+            className="mainAnswer"
+            style={{ color: "#191f25", textAlign: "left" }}
+          >
             ZNX is a coin, native token on Zilionixx blockchain technology.
           </span>
         </div>
       </div>
-      <div style={{ marginTop: "120px" }} />
+      <div style={{ marginTop: "40px" }} />
       <Row className="our-background">
         <Col className="c-column" xs="12" md="6" lg="4">
-          <h3 className="title-h3" style={{ marginBottom: "23px" }}>
-            Zilionixx Protocol
-          </h3>
+          <div
+            className="c-introduceContainer"
+            style={{ justifyContent: "left" }}
+          >
+            <div className="c-number">01</div>
+            <h3 className="title-h3" style={{ marginBottom: "23px" }}>
+              Zilionixx Protocol
+            </h3>
+          </div>
           <div className="our-card-content">
             Zilionixx is being developed as the next-generation blockchain that
             ensures safer and faster transaction processing speed by completing
@@ -32,10 +42,16 @@ const OurTokens = () => {
             hashchain and DAG.
           </div>
           <div style={{ marginTop: "100px" }} />
+          <div
+            className="c-introduceContainer"
+            style={{ justifyContent: "left" }}
+          >
+            <div className="c-number">03</div>
+            <h3 className="title-h3" style={{ marginBottom: "23px" }}>
+              Unicial & Meditech
+            </h3>
+          </div>
 
-          <h3 className="title-h3" style={{ marginBottom: "23px" }}>
-            Unicial & Meditech
-          </h3>
           <div className="our-card-content">
             We have built a virtual world in Zilionixx and are developing
             services that guarantee various social activities, such as medical
@@ -47,23 +63,36 @@ const OurTokens = () => {
           className="c-column"
           xs="12"
           md="6"
-          lg={{ offset: 4, size: 4 }}
+          // lg={{ offset: 4, size: 4 }}
           style={{ textAlign: "right" }}
         >
-          <h3 className="title-h3" style={{ marginBottom: "23px" }}>
-            AI Smart Contract
-          </h3>
+          <div
+            className="c-introduceContainer"
+            style={{ justifyContent: "right" }}
+          >
+            <div className="c-number">02</div>
+            <h3 className="title-h3" style={{ marginBottom: "23px" }}>
+              AI Smart Contract
+            </h3>
+          </div>
+
           <div className="our-card-content" style={{ marginLeft: "auto" }}>
             By introducing AI into smart contracts, we are developing smart
             contract VMs that will be more useful in Unicial and other dApps by
             realizing the combination of Zilioxx blockchain data and AI.
           </div>
           <div style={{ marginTop: "100px" }} />
+          <div
+            className="c-introduceContainer"
+            style={{ justifyContent: "right" }}
+          >
+            <div className="c-number">04</div>
+            <h3 className="title-h3" style={{ marginBottom: "23px" }}>
+              Interact with <br />
+              Game
+            </h3>
+          </div>
 
-          <h3 className="title-h3" style={{ marginBottom: "23px" }}>
-            Interact with <br />
-            Game
-          </h3>
           <div className="our-card-content" style={{ marginLeft: "auto" }}>
             We are trying to develop an SDK that can be used easily by game
             developers by improving transaction processing speed on blockchain
